8f0fe939084a2a164f75639ee422c07bbda40819
midavgigo <ddd20162004@gmail.com>
2023-02-22 15:00:44 +0300

    Начал делать Playlist класс

---
+++ Playlist.cpp
@@ -0,0 +1,13 @@
+#include "Playlist.h"
+
+void Playlist::Play(){
+    player.setState(true);
+}
+
+void Playlist::Pause(){
+    player.setState(false);
+}
+
+void Playlist::AddSong(Song song){
+    Node 
+}
--- inc/Playlist.h
+++ inc/Playlist.h
@@ -1,14 +1,30 @@
 #ifndef __PLATLIST_H__
 #define __PLATLIST_H__
 #include "Song.h"
+#include "Player.h"
+#include <thread>
 
 class Node{
     Node *prev, *next;
     Song song;
+    Node(Song _song, Node *_prev, Node *_next){
+        song = _song;
+        prev = _prev;
+        next = _next;
+    }
 };
 
 class Playlist{
-
+private:
+    Node first;
+    Node *now;
+    Player player;
+public:
+    Playlist();
+    void Play();
+    void Pause();
+    void AddSong(Song song);
+    void Next();
 };
 
 #endif
